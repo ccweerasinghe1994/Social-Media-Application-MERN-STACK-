cc3fc8b8adcb89dc720f7cf77e171042c72f0aba
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-08-26 13:58:16 +0530

    finishing the userById controller :smile:

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -22,14 +22,34 @@ const list = async (req, res) => {
 
     try {
         let users = await User.find().select('name email updated created');
-         res.json(users);
-    }catch (err){
+        res.json(users);
+    } catch (err) {
         return res.status(400).json({
-            error:errorHandler.getErrorMessage(err)
+            error: errorHandler.getErrorMessage(err)
         })
     }
 }
 const userByID = (req, res, next, id) => {
+    try {
+        //If a matching user is found in the database, the user object is appended to the request
+        // object in the profile key. Then, the next() middleware is used to propagate control
+        // to the next relevant controller function. For example, if the original request was to
+        // read a user profile, the next() call in userByID would go to the read controller
+        // function, which is discussed next.
+        let user = User.findById(id);
+        if (!user) {
+            return res.status('400').json({
+                error: 'User not found'
+            })
+        }
+        req.profile = user;
+        next();
+
+    } catch (err) {
+        return res.status('400').json({
+            error: "Could not retrieve the user"
+        })
+    }
 }
 const read = (req, res) => {
 }
