7117ea2542fb7c553734c765af7cdf40e89c4dcb
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-08-26 14:18:40 +0530

    updating the user :rocket:

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -51,9 +51,40 @@ const userByID = (req, res, next, id) => {
         })
     }
 }
+//When the Express app gets a GET request at '/api/users/:userId', it executes the
+// userByID controller function to load the user by the userId value, followed by the
+// read controller function.
 const read = (req, res) => {
+    //The read function retrieves the user details from req.profile and removes
+    // sensitive information, such as the hashed_password and salt values, before
+    // sending the user object in the response to the requesting client.
+    req.profile.hashed_password = undefined;
+    req.profile.salt = undefined;
+
+    return res.json(req.profile)
 }
-const update = (req, res, next) => {
+//The update function retrieves the user details from req.profile and then uses the
+// lodash module to extend and merge the changes that came in the request body to
+// update the user data. Before saving this updated user to the database, the updated
+// field is populated with the current date to reflect the last updated timestamp. Upon
+// successfully saving this update, the updated user object is cleaned by removing
+// sensitive data, such as hashed_password and salt, before sending the user object in
+// the response to the requesting client.
+const update = async (req, res, next) => {
+    try {
+
+        let user = req.profile;
+        user = extend(user, req.body);
+        user.updated = Date.now();
+        await user.save();
+        user.hashed_password = undefined;
+        user.salt = undefined;
+        return res.json(user)
+    } catch (err) {
+        return res.status('400').json({
+            error: errorHandler.getErrorMessage(err)
+        })
+    }
 }
 const remove = (req, res, next) => {
 }
