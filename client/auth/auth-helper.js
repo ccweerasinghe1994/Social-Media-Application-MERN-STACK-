56118c26b4b586aa5cdb21bdafecae722016ab18
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-09-03 08:53:38 +0530

    Update auth-helper.js
    add authenticate method :pear:  add isAuthenticated method :dog2:  and clearJwt mehord to clear the sessionStorage data

--- client/auth/auth-helper.js
+++ client/auth/auth-helper.js
@@ -1,8 +1,33 @@
-const authenticate = (jwt,cb) => {
+import {signout} from "./api-auth";
 
-    if(typeof window !== "undefined"){
-        sessionStorage.setItem('jwt',JSON.stringify(jwt))
+const authenticate = (jwt, cb) => {
+
+    if (typeof window !== "undefined") {
+        sessionStorage.setItem('jwt', JSON.stringify(jwt))
     }
     cb();
+
+}
+
+const isAuthenticated = () => {
+    if (typeof window === 'undefined') {
+        return false
+    }
+    if (sessionStorage.getItem('jwt')) {
+        return JSON.parse(sessionStorage.getItem('jwt'))
+    }
+    return false
+}
+
+const clearJwt = (cb) => {
+    if (typeof window !== 'undefined') {
+        sessionStorage.removeItem('jwt')
+    }
+    cb();
+    signout().then((data) => {
+        document.cookie = "t=; expires=Thu, 01 Jan 1970\n" +
+            "00:00:00\n" +
+            "UTC; path=/;\""
+    })
 
 }
