5bcd18420c88a53f921d70ddc112ffc1d15f7a18
wchamara <chamara.weerasinghe@my.sliit.lk>
2020-09-03 10:23:28 +0530

    add all usrs to the front end :factory:

--- client/MainRouter.js
+++ client/MainRouter.js
@@ -1,12 +1,14 @@
 import React from 'react'
 import {Switch, Route} from 'react-router-dom';
 import Home from './core/Home'
+import {User} from "./user/Users.component";
 
 const MainRouter = () => {
     return (
         <div>
             <Switch>
                 <Route exact  path='/' component={Home}/>
+                <Route path='/users' component={User}/>
             </Switch>
         </div>
     )
---
+++ client/user/Users.component.js
@@ -0,0 +1,78 @@
+import React from 'react';
+import {useState, useEffect} from "react";
+import {list} from "./api-user";
+import {makeStyles} from '@material-ui/core/styles';
+import {
+    Paper,
+    Typography,
+    List,
+    ListItem,
+    Avatar,
+    ListItemAvatar,
+    ListItemText,
+    ListItemSecondaryAction,
+    IconButton
+} from "@material-ui/core";
+import {Link} from "react-router-dom";
+import {Person, ArrowForward} from "@material-ui/icons";
+
+const useStyles = makeStyles(theme => ({
+    title: {
+        padding: `${theme.spacing(3)}px ${theme.spacing(2.5)}px ${theme.spacing(2)}px`,
+        color: theme.palette.openTitle
+    }
+}))
+
+export const User = () => {
+
+
+    const [users, setUsers] = useState([]);
+
+    useEffect(
+        () => {
+            const abortController = new AbortController();
+            const signal = abortController.signal;
+            list(signal).then((data) => {
+                if (data && data.error) {
+                    console.log(data.error)
+                } else {
+                    setUsers(data)
+                }
+            })
+            return function cleanUp() {
+                abortController.abort();
+            }
+        }, []
+    );
+    const {root, title} = useStyles();
+    return (
+        <Paper classes={root} elevation={4}>
+            <Typography variant='h6' className={title}>All Users</Typography>
+            <List dense>
+                {
+                    users.map((item, i) => {
+                        return (
+                            <Link to={"/users/" + item._id} key={i}>
+                                <ListItem button>
+                                    <ListItemAvatar>
+                                        <Avatar>
+                                            <Person/>
+                                        </Avatar>
+                                    </ListItemAvatar>
+                                    <ListItemText primary={item.name}/>
+                                    <ListItemSecondaryAction>
+                                        <IconButton>
+                                            <ArrowForward/>
+                                        </IconButton>
+                                    </ListItemSecondaryAction>
+
+
+                                </ListItem>
+                            </Link>
+                        )
+                    })
+                }
+            </List>
+        </Paper>
+    )
+}
